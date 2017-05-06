c7dd4dc02c35dc87097059050f92358f38087b4f
Fabio Vides F <videsf@uninorte.edu.co>
2017-05-06 17:08:28 -0500

    Some minor changes

--- server.js
+++ server.js
@@ -1,31 +1,68 @@
- 'use strict';
+/*
+¿Qué hace este código? Muy sencillo, las primeras líneas se encargan de incluir las dependencias que vamos a usar,
+ algo así como los includes en C o PHP, o los import de Python. Importamos Express para facilitarnos crear el servidor y 
+ realizr llamadas HTTP. Con http creamos el servidor que posteriormente escuchará en el puerto 3000 de nuestro ordenador
+  (O el que nosotros definamos).
+Con bodyParser permitimos que pueda parsear JSON, methodOverride() nos permite implementar y personalizar métodos HTTP.
+Podemos declarar las rutas con app.route(nombre_de_la_ruta) seguido de los verbos .get(), .post(), etc… y 
+podemos crear una instancia para ellas con express.Router()
+*/
 
-var port = process.env.PORT || 8000;
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+// Connection to DB
 
- var http = require('http');
- var express = require('express');
- var bodyParser = require('body-parser');
- var swaggerize = require('swaggerize-express');
- var swaggerUi = require('swaggerize-ui'); // change one
- var path = require('path');
+mongoose.connect('mongodb://cmsmongo:52Nk2hihQgwWgN8R6h9VeKEUlKdq9LU9Hl2BjJobX12eZN3bHZt4LAPr8wEVm7wbfoo2QETapqXl68HOGLJv6g==@cmsmongo.documents.azure.com:10250/CMSData/?ssl=true', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
 
- var app = express();
 
- var server = http.createServer(app);
 
- app.use(bodyParser.json());
+// Middlewares
 
- app.use(swaggerize({
-     api: path.resolve('./config/api.json'), // change two
-     handlers: path.resolve('./handlers'),
-     docspath: '/swagger/docs/v1' // change three
- }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
- // change four
- app.use('/swagger', swaggerUi({
-   docs: '/swagger/docs/v1'  
- }));
+app.use(bodyParser.json());
 
- server.listen(port, function () {
-     app.setHost(undefined); // change five
- });
+app.use(methodOverride());
+
+
+
+// Import Models and controllers
+
+var models     = require('./Models/User')(app, mongoose);
+
+var UserCtrl = require('./Controllers/UsersController');
+
+// Example Route
+
+var router = express.Router();
+router.get('/', function(req, res) {
+  res.send("Hello world!");
+});
+app.use(router);
+
+// API routes
+var myusers = express.Router();
+myusers.route('/users')
+
+  .get(UserCtrl.findAllUsers)
+
+  .post(UserCtrl.addUser);
+
+
+app.use('/api', myusers);
+
+
+
+// Start server
+
+app.listen(3000, function() {
+
+  console.log("Node server running on http://localhost:3000");
+
+});
